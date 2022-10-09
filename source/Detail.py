a62669e2c19411974978dc6e01e95dfe59545cc6
zhang.ryan <55484667+hz157@users.noreply.github.com>
2022-10-09 14:25:49 +0800

    get article data

---
+++ source/Detail.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+"""
+@Project: Weibo Spider
+@File: Detail.py
+@Author: RyanZhang
+@Date: 2022-10-08
+"""
+
+
+from bs4 import BeautifulSoup
+import requests
+import json
+
+
+def getArt(mid, cookie):
+    print("Parsing: {}".format(mid))
+    url = "https://m.weibo.cn/status/{}".format(mid)
+    head = {
+        "User-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Mobile/15E148 Safari/604.1",
+        "accept": "application/json, text/plain, */*",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "zh-CN,zh;q=0.9",
+        "cookie": cookie
+    }
+    session = requests.Session()
+    res = session.get(url, headers=head, timeout=10)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    script = soup.find_all('script')[2].text
+    temps = script.split("var $render_data = [")[1].split("][0] || {};")[0]
+    jsonData = json.loads(temps)
+    # Data Split
+    userName = jsonData.get("status").get("user").get("screen_name")
+    verified = jsonData.get("status").get("user").get("verified")
+    verifiedType = jsonData.get("status").get("user").get("verified_type")
+    verifiedReason = jsonData.get("status").get("user").get("verified_reason")
+    content = jsonData.get("status").get("text")
+    picList = jsonData.get("status").get("pic_ids")
+    # result list
+    result = [mid, userName, verified, verifiedType, verifiedReason, content, picList]
+    return result
+
+
+
+
