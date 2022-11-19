f025137a1c087f9331211fc478a2b681d8d66f1f
hz157 <horizon221@foxmail.com>
2022-11-19 21:04:41 +0800

    配合Mysql时间格式修改

--- source/ConvertTime.py
+++ source/ConvertTime.py
@@ -3,4 +3,4 @@ from datetime import datetime
 
 def convert(date):
     cat = datetime.strptime(date, "%a %b %d %H:%M:%S %z %Y")
-    return cat.strftime("%Y-%m-%d %H:%M:%S %Z")
+    return cat.strftime("%Y-%m-%d %H:%M:%S")
